316991f9c081d019c3fcda72aac0cfd399223051
towsatt <140756061+towsatt@users.noreply.github.com>
2023-10-31 16:45:49 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minifb::{ScaleMode, Window, WindowOptions, Key};
 mod elements;
 
 fn main() {
-    let mut buffer = [0; 640*640];
+    let mut buffer = [0; 160*160];
     let sprites = get_sprites();
     let mut window = Window::new(
         "Test - ESC to exit",
-        640,
-        640,
+        160,
+        160,
         WindowOptions::default(),
     )
     .unwrap();
@@ -21,13 +21,13 @@ fn main() {
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in 0..16{
             for j in 0..16{
-                buffer[i*640 + j] = sprites["pacman_right_1"][i*16+j];
+                buffer[i*160 + j] = sprites["pacman_right_1"][i*16+j];
             }
         }
         
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
-        .update_with_buffer(&buffer, 640, 640)
+        .update_with_buffer(&buffer, 160, 160)
             .unwrap();
     }
 }
