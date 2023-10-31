cdd7339949cc56a6c9619400c445d716770c3292
towsatt <140756061+towsatt@users.noreply.github.com>
2023-10-31 16:40:50 +0000

    .

--- src/elements.rs
+++ src/elements.rs
@@ -1,18 +1,46 @@
-use std::fs::File;
+use std::{collections::HashMap, fs::File};
 
-pub fn get_sprites(){
-    
-}
+use png::Decoder;
 
-fn get_buffer(name: &str) -> Vec<u32> {
-    let decoder = png::Decoder::new(File::open(format!("assets/{name}.png")).unwrap());
+pub fn get_sprites() -> HashMap<String, Vec<u32>> {
+    let mut hash: HashMap<String, Vec<u32>> = HashMap::new();
+    let decoder = Decoder::new(File::open("sprites.png").unwrap());
     let mut reader = decoder.read_info().unwrap();
     let mut buffer = vec![0; reader.output_buffer_size()];
     let info = reader.next_frame(&mut buffer).unwrap();
-    println!("{:?}", info);
-    let u32_buffer: Vec<u32> = buffer
-        .chunks_exact(4)
-        .map(|v| ((v[0] as u32) << 16 | (v[1] as u32) << 8 | v[2] as u32))
+    let bytes = &buffer[..info.buffer_size()];
+    let vector: Vec<u32> = bytes
+        .chunks(4)
+        .map(|v| (v[0] as u32) << 16 | (v[1] as u32) << 8 | (v[2] as u32))
         .collect();
-    u32_buffer
+
+    let mut helper = |name, x, y| hash.insert(name, crop(&vector, (x, y)));
+    for direction in 0..4 {
+        for x in 0..2 {
+            helper(
+                format!(
+                    "pacman_{:?}_{:?}",
+                    ["right", "left", "up", "down"][direction],
+                    x + 1
+                ),
+                x,
+                direction,
+            );
+        }
+    }
+    hash
+}
+
+pub fn crop(image: &[u32], sprite: (usize, usize)) -> Vec<u32> {
+    let (width, height) = (16, 16);
+    let (x, y) = sprite;
+
+    let mut dest = vec![];
+    for i in 0..height {
+        let start = (i + y) * height + (x * width);
+        let end = (i + y) * height + ((x + 1) * width);
+        dest.extend_from_slice(&image[start..end]);
+    }
+
+    dest
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use std::fs::File;
 
+use elements::get_sprites;
 use minifb::{ScaleMode, Window, WindowOptions, Key};
 
 mod elements;
 
 fn main() {
-
+    let mut buffer = [0; 640*640];
+    let mut sprites = get_sprites();
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        640,
+        640,
+        WindowOptions::default(),
+    )
+    .unwrap();
+// Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in 0..16{
+            for j in 0..16{
+                buffer[j*640 + i] = sprites["pacman_right_1"][j*16+i];
+            }
+        }
+        
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+        .update_with_buffer(&buffer, 640, 640)
+            .unwrap();
+    }
 }
