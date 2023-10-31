def3161dc287bb0f8c4f29f96edef7733b250efa
towsatt <140756061+towsatt@users.noreply.github.com>
2023-10-31 14:17:29 +0000

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::File;
+
+use minifb::{ScaleMode, Window, WindowOptions, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let decoder = png::Decoder::new(File::open("textures/assets.png").unwrap());
+    let mut reader = decoder.read_info().unwrap();
+    let mut buffer = vec![0; reader.output_buffer_size()];
+    reader.next_frame(&mut buffer).unwrap();
+    let u32_buffer: Vec<u32> = buffer
+        .chunks_exact(3)
+        .map(|v| ((v[0] as u32) << 16 | (v[1] as u32) << 8 | v[2] as u32))
+        .collect();
+    let mut window = Window::new(
+        "assets",
+        reader.info().width as usize,
+        reader.info().height as usize,
+        WindowOptions {
+            resize: false,
+            scale_mode: ScaleMode::Center,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("Unable to open Window");
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window
+            .update_with_buffer(
+                &u32_buffer,
+                reader.info().width as usize,
+                reader.info().height as usize,
+            )
+            .unwrap();
+    }
 }
